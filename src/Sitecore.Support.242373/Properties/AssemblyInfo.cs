871c4fa732102ca173ef78e0ac29a5ec5fdebcd2
DK\ala <art.alexeyenko@gmail.com>
2018-12-05 14:35:24 -0500

    Backporting official fix to 3.7.1

---
+++ src/Sitecore.Support.242373/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.242373")]
+[assembly: AssemblyProduct("Sitecore.Support.242373")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.242373/XA/Foundation/Theming/Bundler/AssetLinksGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.XA.Foundation.Theming.Configuration;
+
+namespace Sitecore.Support.XA.Foundation.Theming.Bundler
+{
+  public class AssetLinksGenerator : Sitecore.XA.Foundation.Theming.Bundler.AssetLinksGenerator
+  {
+    private readonly AssetConfiguration _configuration;
+
+    public AssetLinksGenerator() : base()
+    {
+      _configuration = AssetConfigurationReader.Read();
+    }
+
+    protected override string GenerateCacheKey(int hash)
+    {
+      var siteHostName = Sitecore.Context.Site.HostName;
+      return $"{DatabaseRepository.GetContentDatabase().Name}#{Sitecore.Context.User.IsAuthenticated}#{_configuration.CacheKey}#{siteHostName}#{hash}";
+    }
+  }
+}
